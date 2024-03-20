2c09056c0caa910370df70731f3a0c96ec5a1916
Adrian McFarlane <confuzedfx@gmail.com>
2024-03-19 22:25:50 -0700

    handler package

---
+++ internal/db/handler/auth.go
@@ -0,0 +1,85 @@
+package handler
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/url"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/confy/godo/internal/db"
+	"golang.org/x/oauth2"
+)
+
+func HandleAuthLogin(oauth *oauth2.Config) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Redirect to the OAuth2 login page
+		http.Redirect(w, r, oauth.AuthCodeURL("state"), http.StatusSeeOther)
+	}
+}
+
+func HandleAuthCallback(
+	session *scs.SessionManager,
+	oauth *oauth2.Config,
+	database *db.Queries) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Get the code from the query string
+		code := r.URL.Query().Get("code")
+		// Exchange the code for a token
+		token, err := oauth.Exchange(context.Background(), code)
+		if err != nil {
+			http.Error(w, "Failed to exchange token", http.StatusInternalServerError)
+			return
+		}
+
+		// Get the user information
+		user, err := getUserInfo(oauth.Client(context.Background(), token))
+		if err != nil {
+			http.Error(w, "Failed to get user information", http.StatusInternalServerError)
+			return
+		}
+
+		// Create or get the user in the database
+		dbUser, err := db.CreateOrGetUser(context.Background(), database, user)
+		if err != nil {
+			http.Error(w, "Failed to create or get user", http.StatusInternalServerError)
+			return
+		}
+
+		// Store user ID and token in the session
+		session.Put(r.Context(), "userID", dbUser.ID)
+		session.Put(r.Context(), "token", token.AccessToken)
+
+		// Redirect to the original URL or home page
+		redirectURL := "/"
+		originalURL := session.PopString(r.Context(), "redirect")
+		if originalURL != "" {
+			redirectURL = originalURL
+		}
+		http.Redirect(w, r, redirectURL, http.StatusSeeOther)
+	}
+}
+
+func getUserInfo(client *http.Client) (db.CreateUserParams, error) {
+	// resp, err := client.Get("https://api.github.com/user")
+	resp, err := client.Do(&http.Request{
+		Method: http.MethodGet,
+		URL:    &url.URL{Scheme: "https", Host: "api.github.com", Path: "/user"},
+	})
+	if err != nil {
+		return db.CreateUserParams{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return db.CreateUserParams{}, err
+	}
+
+	var user db.CreateUserParams
+	err = json.NewDecoder(resp.Body).Decode(&user)
+	if err != nil {
+		return db.CreateUserParams{}, err
+	}
+
+	return user, nil
+}
